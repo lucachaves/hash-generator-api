3ea5e021b9b2849f18a96e710a5a357f0dde8518
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2016-01-22 10:08:06 -0300

    rotas para gerar e obter tipos de hash

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
 var express = require('express');
 var crypto = require('crypto');
-var cors = require('cors')
+var cors = require('cors');
+var exec = require('child_process').exec;
 var app = express();
-app.use(cors())
+app.use(cors());
 
-// type sha1, sha256, sha512, md4, md5
-//   openssl list-message-digest-algorithms
-app.get('/:type/:data', function (req, res) {
-      console.log(req.params.type);
-      console.log(req.params.data);
-      var hash = {
-        "kind": req.params.type,
-        "string": req.params.data,
-        "hash": crypto.createHash(req.params.type).update(req.params.data).digest("hex")
+// TODO try error (invalid param)
+
+/**
+ * Generate hash by type
+ * @param {String} type
+ * @param {String} data
+ */
+app.get('/hash/:type/:data', function (req, res) {
+  var type = req.params.type;
+  var data = req.params.data;
+  var digest = crypto.createHash(type).update(data).digest("hex")
+  var hash = {
+    "kind": type,
+    "string": data,
+    "hash": digest
+  };
+  setTimeout(function(){
+    res.json(hash);
+  },2000);
+});
+
+/**
+ * Get all hash types
+ */
+app.get('/hash/types', function (req, res) {
+  var messageDigest = '';
+  var child = exec('openssl list-message-digest-commands',
+    function (error, stdout, stderr) {
+      messageDigest = stdout;
+      messageDigest = messageDigest.split("\n")
+      messageDigest.pop();
+      var types = {
+        "types": messageDigest
       };
       setTimeout(function(){
-        res.json(hash);
+        res.json(types);
       },2000);
+  });
 });
 
 app.listen(5000, function () {
-  console.log('open http://localhost:5000/md5/luiz');
+  console.log('hash generator web api: http://localhost:5000/');
 });
